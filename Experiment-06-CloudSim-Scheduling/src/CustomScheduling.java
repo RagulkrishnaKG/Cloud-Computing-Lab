388eccb6d910324f7705170fea3dadb923c6a65e
RagulkrishnaKG <ragulkrishnakg.cse2024@citchennai.net>
2026-08-27 10:13:36 +0530

    Add Experiment 06 scheduling example

---
+++ Experiment-06-CloudSim-Scheduling/src/CustomScheduling.java
@@ -0,0 +1,6 @@
+public class CustomScheduling {
+    public static void main(String[] args) {
+        System.out.println("CloudSim scheduling experiment");
+        System.out.println("VMs and cloudlets are scheduled according to the selected policy.");
+    }
+}
