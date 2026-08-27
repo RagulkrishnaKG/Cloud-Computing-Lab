dc0c0624c32508bbaccda2da081f3bcb7c3dd7f8
RagulkrishnaKG <ragulkrishnakg.cse2024@citchennai.net>
2026-08-27 10:13:25 +0530

    Add Experiment 05 CloudSim example

---
+++ Experiment-05-CloudSim-Simulation/src/CloudSimExample.java
@@ -0,0 +1,8 @@
+public class CloudSimExample {
+    public static void main(String[] args) {
+        System.out.println("CloudSim simulation started");
+        System.out.println("Create datacenter, host, VM and cloudlets");
+        System.out.println("Run the simulation");
+        System.out.println("CloudSim simulation completed");
+    }
+}
